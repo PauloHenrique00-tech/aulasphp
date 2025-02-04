9cde598858444a3873528d128dd009109b6817a2
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-02-04 11:42:45 -0300

    Adiciona condicional SIMPLES

---
+++ 05-condicionais.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Condicionais</title>
+    <hr>
+
+    <h2>Simples</h2>
+    <h3><code>if</code></h3>
+<?php
+$numero = 5;
+
+if($numero > 1){
+    echo "<p>$numero é maior que 1</p>";
+}
+
+// Sintaxe omitindo as chaves
+if($numero > 1) echo "<p>$numero é maior que 1</p>";
+?>
+
+
+    <h2>Composta</h2>
+
+    <h2>Encadeada</h2>
+
+</head>
+<body>
+    
+</body>
+</html>

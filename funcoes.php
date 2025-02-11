b4589b477ea4419f97afe638f1ac9c6693ccb8d4
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-02-11 10:27:41 -0300

    Ajusta comentário php

--- funcoes.php
+++ funcoes.php
@@ -120,8 +120,11 @@ object -> objeto */
 
         <!-- O código abaixo (tire os comentários se quiser testar) gera um erro relacionado a indução de tipos (foi dado uma string e era esperado um número inteiro)
         <p>Teste de valor/parâmetro errado: 
-            <?=verificarNegativo("Paulo Henrique")?></p> 
+            <//?=verificarNegativo("Paulo Henrique")?></p> 
             -->
+
+            <hr>
+
     </div>    
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>

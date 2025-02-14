652988eb6e608ea364dfd111df39a378ab116403
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-02-14 08:37:45 -0300

    Cria página dúvidas

---
+++ 11-file/duvidas.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dúvidas - Site-PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>Site com PHP</h1>
+            <nav>
+                <a href="index.php">Home</a>
+                <a href="cursos.php">Cursos</a>
+                <a href="duvidas.php">Dúvidas</a>
+            </nav>
+        </header>
+
+        <main>
+            <h2>Página de dúvidas</h2>
+            <p>Esta é a primeira página do nosso site.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In culpa minus tenetur sint illo qui rerum accusamus, molestias laudantium sequi quis architecto itaque, quisquam commodi dolores velit quasi assumenda beatae.</p>
+
+            <h3>Principais Dúvidas</h3>
+            <details>
+                <summary>Como se cadastrar</summary>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt iste porro autem nobis tempora vero eveniet blanditiis. Rem, deleniti mollitia officia voluptatum sequi quod, in nihil alias numquam repellat consectetur.</p>
+            </details>
+            <details>
+                <summary>Os cursos são gratuitos</summary>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quaerat, illum incidunt aliquam dolorem in quod libero iusto repellat. Velit natus mollitia accusantium aspernatur distinctio, porro voluptatum ullam officia ad.</p>
+            </details>
+        </main>
+        <footer>
+            <p>Site criado para o curso Téc. Informática para Internet</p>
+        </footer>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
